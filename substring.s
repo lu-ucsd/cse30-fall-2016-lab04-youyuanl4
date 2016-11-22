bdba08ad42608a9fc63ef1b987f73f20b645cff3
Youyuan Lu <yol093@ucsd.edu>
2016-11-22 00:30:02 +0000

    3.1 completed

--- substring.s
+++ substring.s
@@ -14,9 +14,70 @@ substring:
     @ YOUR CODE GOES HERE (char *str1 is in r0, char *str2 is in r1)
     @-----------------------
 
-    @ (your code)
+    CMP r0, #0
+    BEQ zero  @ when str1 is NULL
+    CMP r1, #0
+    BEQ zero  @ when str2 is NULL
 
-    @ put your return value in r0 here:
+    PUSH {r0-r3}
+    BL strlen
+    MOV r4, r0  @ r4 = strlen(str1)
+    POP {r0-r3}
+
+    PUSH {r0-r3}
+    MOV r0, r1
+    BL strlen
+    MOV r5, r0  @ r5 = strlen(str2)
+    POP {r0-r3}
+
+    CMP r4, #0
+    BEQ one  @ when strlen(str1) == 0
+    CMP r5, #0
+    BEQ one  @ when strlen(str2) == 0
+
+    @ make r0 the possible substring, and r4 its length
+    CMP r4, r5
+    MOVGT r6, r0  @ swap r0 and r1
+    MOVGT r0, r1
+    MOVGT r1, r6
+    MOVGT r6, r4  @ swap r4 and r5
+    MOVGT r4, r5
+    MOVGT r5, r6
+
+    MOV r6, #0
+    SUB r5, r5, r4
+@ use outer for loop to traverse all substrings of length r4 in r1
+for:
+    CMP r6, r5
+    BGT zero
+
+    MOV r7, #0
+    MOV r11, r6
+@ use inner loop to examine every substrings
+for2:
+    CMP r7, r4
+    BEQ one  @ when r0 is found in r1
+    LDRB r8, [r0, r7]
+    LDRB r9, [r1, r6]
+    CMP r8, r9
+    BNE endfor2
+    ADD r7, r7, #1
+    ADD r6, r6, #1
+    B for2
+
+endfor2:
+    MOV r6, r11
+    ADD r6, r6, #1
+    B for
+
+zero:
+    MOV r0, #0
+    B end
+
+one:
+    MOV r0, #1
+
+end:
 
     @-----------------------
 
--- substring_test.c
+++ substring_test.c
@@ -4,8 +4,44 @@
 
 #include <stdio.h>
 #include "substring.h"
+#include <assert.h>
 
 int main() {
-    // put your test code here
+/* test the ARM implementation of substring() */
+    char * str0 = NULL;
+    char * str1 = "";
+    char * str2 = "cse";
+    char * str3 = "eecsce";
+    char * str4 = "cse30";
+    char * str5 = "cs26cse";
+
+    /* NULL cases */
+    assert(substring(str0, str0) == 0);
+    assert(substring(str0, str1) == 0);
+    assert(substring(str1, str0) == 0);
+    assert(substring(str0, str2) == 0);
+    assert(substring(str2, str0) == 0);
+
+    /* empty string cases */
+    assert(substring(str1, str1) == 1);
+    assert(substring(str1, str2) == 1);
+    assert(substring(str2, str1) == 1);
+
+    /* random cases */
+    assert(substring(str2, str2) == 1);
+    assert(substring(str2, str3) == 0);
+    assert(substring(str3, str2) == 0);
+    assert(substring(str2, str4) == 1);
+    assert(substring(str4, str2) == 1);
+    assert(substring(str2, str5) == 1);
+    assert(substring(str5, str2) == 1);
+
+    /* random cases */
+    assert(substring(str3, str3) == 1);
+    assert(substring(str3, str4) == 0);
+    assert(substring(str4, str3) == 0);
+
+    puts("Passed All Test Cases for substring");
+
     return 0;
 }
