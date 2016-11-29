12384c68e9076a1c3b869291247e9e67864689fe
Youyuan Lu <yol093@ucsd.edu>
2016-11-29 08:52:16 +0000

    4.2 completed

--- binary_search_ARM.s
+++ binary_search_ARM.s
@@ -20,9 +20,29 @@ binary_search_ARM:
     push    {r4-r11, ip, lr}
     @ May need to decrement stack pointer for more stack space
 
-    /* You should probably do something here */
+    @ r4 = mid = start + (end - start) / 2
+    SUB r4, r3, r2
+    LSR r4, r4, #1
+    ADD r4, r4, r2
 
-    mov r0, #0
+    @ if (start > end)
+    CMP r2, r3
+    MOVGT r0, #-1
+    BGT end
+
+    @ r5 = data[mid]
+    LDR r5, [r0, r4, LSL #2]
+
+    @ if (data[mid] == toFInd)
+    CMP r5, r1
+    MOVEQ r0, r4
+    BEQ end
+
+    SUBGT r3, r4, #1  @ if (data[mid] > toFInd)
+    ADDLT r2, r4, #1  @ if (data[mid] < toFInd)
+    BL binary_search_ARM
+
+end:
     @ Remember to restore the stack pointer before popping!
     @ This handles restoring registers and returning
     pop     {r4-r11, ip, pc}
--- binary_search_test.c
+++ binary_search_test.c
@@ -4,11 +4,28 @@
 
 #include <stdio.h>
 #include "binary_search.h"
+#include <assert.h>
 
 int main(int argc, char ** argv) {
-    int data[] = {1,2,3,4,5,6,7,8,9,10};
+  /* tester for binary_search_ARM */
+  int data[] = {1,2,3,4,5,6,7,8,9,10};
 
-    printf("C   Binary Search: %d\n", binary_search(data, 4, 0, 9));
-    printf("ARM Binary Search: %d\n", binary_search_ARM(data, 4, 0, 9));
-    return 0;
+  /* -1 cases */
+  assert(binary_search(data,4,0,0) == binary_search_ARM(data,4,0,0));
+  assert(binary_search(data,4,0,11) == binary_search_ARM(data,4,0,11));
+
+  /* list element cases */
+  assert(binary_search(data,4,0,1) == binary_search_ARM(data,4,0,1));
+  assert(binary_search(data,4,0,2) == binary_search_ARM(data,4,0,2));
+  assert(binary_search(data,4,0,3) == binary_search_ARM(data,4,0,3));
+  assert(binary_search(data,4,0,4) == binary_search_ARM(data,4,0,4));
+  assert(binary_search(data,4,0,5) == binary_search_ARM(data,4,0,5));
+  assert(binary_search(data,4,0,6) == binary_search_ARM(data,4,0,6));
+  assert(binary_search(data,4,0,7) == binary_search_ARM(data,4,0,7));
+  assert(binary_search(data,4,0,8) == binary_search_ARM(data,4,0,8));
+  assert(binary_search(data,4,0,9) == binary_search_ARM(data,4,0,9));
+  assert(binary_search(data,4,0,10) == binary_search_ARM(data,4,0,10));
+
+  puts("Passed All Test Cases for binary_search_ARM");
+  return 0;
 }
