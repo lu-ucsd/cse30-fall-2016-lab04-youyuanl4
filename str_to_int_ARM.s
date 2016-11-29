7b379d2a1b82b5f329131ffcea7f0bd5748ca302
Youyuan Lu <yol093@ucsd.edu>
2016-11-29 05:15:24 +0000

    3.2 ARM

--- str_to_int_ARM.s
+++ str_to_int_ARM.s
@@ -21,10 +21,82 @@ str_to_int:
     @ We need to save away a bunch of registers
     push    {r4-r11, ip, lr}
 
-    /* Your code goes here */
+    @ invalid arguments
+    CMP r0, #0
+    BEQ zero
+    CMP r1, #0
+    BEQ zero
+    LDRB r4, [r0]
+    CMP r4, #0
+    BEQ zero
 
-    /* Return back to calling function */
-    mov r0, #0
+    MOV r11, #0  @ used to store the resulting integer
+
+    @ if(*s == '-' && *(s + 1) != '\0')
+    CMP r4, #45
+    BNE else
+    LDRB r4, [r0, #1]
+    CMP r4, #0
+    BEQ else
+
+    MOV r4, #1
+@ use a while loop to examine all characters
+while:
+    LDRB r5, [r0, r4]
+    CMP r5, #0
+    BEQ afterwhile  @ successful translation
+    CMP r5, #48
+    BLT zero  @ invalid characters
+    CMP r5, #57
+    BGT zero  @ invalid characters
+
+    @ update the integer value
+    LSL r10, r11, #3
+    LSL r11, r11, #1
+    ADD r11, r11, r10
+    ADD r11, r11, r5
+    SUB r11, r11, #48
+    ADD r4, r4, #1
+
+    B while
+
+@ get the true negative value
+afterwhile:
+    MVN r11, r11
+    ADD r11, #1
+    B one
+
+else:
+    MOV r4, #0
+@ use a while loop to examine all characters
+while2:
+    LDRB r5, [r0, r4]
+    CMP r5, #0
+    BEQ one  @ successful translation
+    CMP r5, #48
+    BLT zero  @ invalid characters
+    CMP r5, #57
+    BGT zero  @ invalid characters
+
+    @ update the integer value
+    LSL r10, r11, #3
+    LSL r11, r11, #1
+    ADD r11, r11, r10
+    ADD r11, r11, r5
+    SUB r11, r11, #48
+    ADD r4, r4, #1
+
+    B while2
+
+one:
+    STR r11, [r1]
+    MOV r0, #1
+    B end
+
+zero:
+    MOV r0, #0
+
+end:
     @ This handles restoring registers and returning
     pop     {r4-r11, ip, pc}
 
