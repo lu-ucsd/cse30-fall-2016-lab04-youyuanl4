1078ffaae35eb11686346facfd32e598ddf49b31
Youyuan Lu <yol093@ucsd.edu>
2016-11-30 03:19:36 +0000

    4.3 completed

--- majority_count_ARM.s
+++ majority_count_ARM.s
@@ -24,9 +24,116 @@ majority_count_ARM:
     push    {r4-r11, ip, lr}
     @ May want to decrement stack pointer for more space
 
-    /* You should probably do something here */
+    PUSH {r3, ip}
 
-    mov r0, #0
+@ if (len == 0)
+    CMP r1, #0
+    MOVEQ r0, #0
+    BEQ end
+
+@ if (len == 1)
+    CMP r1, #1
+    BNE after1
+    CMP r2, #0
+    LDRNE r3, [r0]
+    STRNE r3, [r2]
+    MOV r0, #1
+    B end
+
+after1:
+@ int left_majority_count = majority_count(arr, len/2, &left_majority)
+    SUB sp, sp, #16
+    STR r0, [sp, #4]
+    STR r1, [sp, #8]
+    STR r2, [sp, #12]
+    LSR r1, r1, #1
+    MOV r2, sp
+    BL majority_count_ARM
+    MOV r4, r0  @ r4 = left_majority_count
+    LDR r3, [sp]  @ r3 = left_majority
+    LDR r0, [sp, #4]
+    LDR r1, [sp, #8]
+    LDR r2, [sp, #12]
+    ADD sp, sp, #16
+    
+@ right_majority_count = majority_count(arr+len/2, len/2, &right_majority)
+    SUB sp, sp, #16
+    STR r0, [sp, #4]
+    STR r1, [sp, #8]
+    STR r2, [sp, #12]
+    LSR r2, r1, #1
+    SUB r1, r1, r2
+    LSL r2, r2, #2
+    ADD r0, r0, r2
+    MOV r2, sp
+    BL majority_count_ARM
+    MOV r6, r0  @ r6 = right_majority_count
+    LDR r5, [sp]  @ r5 = right_majority
+    LDR r0, [sp, #4]
+    LDR r1, [sp, #8]
+    LDR r2, [sp, #12]
+    ADD sp, sp, #16
+
+@ if (left_majority_count)
+    CMP r4, #0
+    BEQ secondif
+    PUSH {r0-r2, ip}
+    MOV r2, r3
+    BL count
+    MOV r7, r0  @ r7 = c = count(arr, len, left_majority)
+    POP {r0-r2, ip}
+    LSL r7, r7, #1
+    CMP r7, r1  @ if (2 * c > len)
+    LSRGT r7, r7, #1
+    BGT storevalue
+
+secondif:
+@ if (right_majority_count)
+    CMP r6, #0
+    MOVEQ r0, #0
+    BEQ end
+    PUSH {r0-r2, ip}
+    MOV r2, r5
+    BL count
+    MOV r7, r0  @ r7 = c = count(arr, len, right_majority)
+    POP {r0-r2, ip}
+    LSL r7, r7, #1
+    CMP r7, r1  @ if (2 * c > len)
+    MOVLE r0, #0
+    BLE end
+    LSR r7, r7, #1
+    MOV r3, r5
+    B storevalue
+
+storevalue:
+@ store the majority element and return its count
+    CMP r2, #0
+    STRNE r3, [r2]
+    MOV r0, r7
+    B end
+
+@ count the number of times the input integer appears in the array
+@ ------------------------------
+count:
+    PUSH {r3-r11, lr}
+    MOV r4, #0  @ r4 = i = 0
+    MOV r5, #0  @ r5 = ret_count = 0
+while:  @ use a while loop to count the accurence of the input integer
+    CMP r4, r1
+    BGE endwhile
+    LDR r6, [r0, r4, LSL #2]
+    CMP r6, r2
+    ADDEQ r5, r5, #1
+    ADD r4, r4, #1
+    B while
+endwhile:
+    MOV r0, r5
+    POP {r3-r11, pc}
+@ ------------------------------
+
+end:
+    POP {r3, ip}
+ 
     @ Remember to restore your stack pointer before popping!
     @ This handles restoring registers and returning
     pop     {r4-r11, ip, pc}
--- majority_count_test.c
+++ majority_count_test.c
@@ -4,18 +4,40 @@
 
 #include <stdio.h>
 #include "majority_count.h"
+#include <assert.h>
 
 int main(int argc, char ** argv) {
+  /* tester for majority_count_ARM */
+  int data[] = {0, 1, 0, 0, 2, 1, 1, 2, 1, 1, 1};
+  int c_majority, arm_majority, c_count, arm_count;
 
-    int data[] = {0, 0, 0, 1};
+  /* null cases */
+  c_count = majority_count(data, 5, NULL);
+  arm_count = majority_count_ARM(data, 5, NULL);
+  assert(c_count == arm_count);
 
-    int c_majority, arm_majority;
-    int c_count = majority_count(data, 4, &c_majority);
-    int arm_count = majority_count_ARM(data, 4, &arm_majority);
+  /* empty array cases */
+  c_count = majority_count(data, 0, &c_majority);
+  arm_count = majority_count_ARM(data, 0, &arm_majority);
+  assert(c_count == arm_count);
 
-    printf("C   Majority Count  : %d\n", c_count);
-    printf("C   Majority Element: %d\n", c_majority);
-    printf("ARM Majority Count  : %d\n", arm_count);
-    printf("ARM Majority Element: %d\n", arm_majority);
-    return 0;
+  /* single element array cases */
+  c_count = majority_count(data, 1, &c_majority);
+  arm_count = majority_count_ARM(data, 1, &arm_majority);
+  assert(c_majority == arm_majority);
+  assert(c_count == arm_count);
+
+  /* random cases */
+  c_count = majority_count(data, 4, &c_majority);
+  arm_count = majority_count_ARM(data, 4, &arm_majority);
+  assert(c_majority == arm_majority);
+  assert(c_count == arm_count);
+
+  c_count = majority_count(data, 11, &c_majority);
+  arm_count = majority_count_ARM(data, 11, &arm_majority);
+  assert(c_majority == arm_majority);
+  assert(c_count == arm_count);
+
+  puts("Passed All Test Cases for majority_count_ARM");
+  return 0;
 }
